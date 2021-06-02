f93856578e0ae8d1083cd27461b723b3b7f2a235
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-06-02 22:05:01 +0700

    reert

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 ﻿const __ENV = process.env.__ENV || 'DEV';
 const __SETTING = require('./setting.json')[__ENV];
 console.log(__SETTING);
-
+//------------------------------------------------------------------------
+const _ = require('lodash');
+const FS = require('fs');
+//------------------------------------------------------------------------
 let grpc = require("grpc");
 var protoLoader = require("@grpc/proto-loader");
 const server = new grpc.Server();
@@ -14,9 +17,69 @@ let packageDefinition = grpc.loadPackageDefinition(
         oneofs: true
     })
 );
+//------------------------------------------------------------------------
+let __id = 0;
+const __ids = [];
+const __caches = [];
+
+function _cacheAdd(v) {
+    if (v == null || typeof v != 'object') return;
+    if (Array.isArray(v)) {
+        for (var i = 0; i < v.length; i++) {
+            __id++;
+            const it = v[i];
+            it.__id = __id;
+            it.__ix = __caches.length;
+            __caches.push(it);
+        }
+    } else {
+        __id++;
+        const it = v[i];
+        it.__id = __id;
+        it.__ix = __caches.length;
+        __caches.push(it);
+    }
+}
+
+function _cacheUpdate(v) {
+    if (v == null || typeof v != 'object') return;
+    if (Array.isArray(v)) {
+        for (var i = 0; i < v.length; i++) {
+            __id++;
+            const it = v[i], ix = __caches.length;
+            it.__id = __id;
+            it.__ix = ix;
+            __caches.push(it);
+            __ids.push(ix);
+        }
+    } else {
+        __id++;
+        const it = v, ix = __caches.length;
+        it.__id = __id;
+        it.__ix = ix;
+        __caches.push(it);
+        __ids.push(ix);
+    }
+}
+function _cacheRemove(v) {
+    if (v == null || typeof v != 'number' || Array.isArray(v) == false) return;
+    if (Array.isArray(v)) {
+        const ixs = _.filter(_.map(__ids, function (x, k) { return _.findIndex(v, function (o) { return o == x }) == -1 ? -1 : k; }), function (j) { return j != -1 });
+        if (ixs.length > 0) {
+            for (var i = 0; i < ixs.length; i++) {
+                __caches[i] = null;
+                __ids[i] = -1;
+            }
+        }
+    } else {
+        __id++;
+        v.__id = __id;
+        __caches.push(v);
+    }
+}
+
 
 //------------------------------------------------------------------------
-
 function _ioLinkUpdate(call, callback) {
     const links = call.request;
 
