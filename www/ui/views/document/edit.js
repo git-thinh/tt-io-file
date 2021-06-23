a5e53fd5cc2b4c04b83803160f59d5ad1df98180
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-06-23 19:51:42 +0700

    heading

--- www/ui/views/document/edit.js
+++ www/ui/views/document/edit.js
@@ -65,6 +65,35 @@
 
             $('*[data-content]').popup();
         },
+        changeImageDefault: function() {
+            var self = this, el = self.$el, view_id = el.getAttribute('id');
+
+            __vcp({
+                code: 'image-select',
+                scope: __scope,
+                popup: true,
+                view_ref: view_id,
+                title: 'Images: ' + self.article.title,
+                class: 'ui overlay fullscreen modal',
+                multi_select: false
+            }, null, function (v) {
+
+            }, function (data) {
+                var imgs = _.map(_.filter(data.images, x => x.select), x => x.src);
+                //console.log('close = ', view_id, imgs, self.article);
+
+                if (imgs.length > 0) {
+                    imgs.forEach(x => {
+                        if (self.article.images.length == 0) self.article.images.push(x);
+                        else self.article.images.unshift(x);
+                    });
+                }
+
+                //$('#' + view_id).modal('show');
+                //$('#' + view_id + ' .dimmer').removeClass('active');
+            });
+
+        },
         updateArticle: function() {
 
         },
@@ -88,7 +117,7 @@
 
                 switch (code) {
                     case 'H':
-                        html += '<h3 cmd-text="{H}" id="' + cid + '" onclick="' + view_id + '.lineSelectChanged(' + cid + ')" class="__line">' + line + '</h3>';
+                        html += '<div cmd-text="{H}" id="' + cid + '" onclick="' + view_id + '.lineSelectChanged(' + cid + ')" class="__line h3">' + line + '</div>';
                         break;
                     default:
                         if (line.endsWith('.jpg') || line.endsWith('.jpeg') || line.endsWith('.png') || line.endsWith('.gif') || line.endsWith('.svg')) {
@@ -113,6 +142,38 @@
             $('.__line').removeClass('active');
             $('#' + pid).addClass('active');
         },
+        domVirtualBuild: function(funcProcess) {
+            var edit = document.getElementById('edit-html');
+            if (edit) {
+                var htmlString = edit.innerHTML;
+                htmlString = htmlString
+                    .split('<p ').join('^<p ')
+                    .split('<br ').join('^<br ')
+                    .split('>').join('>^');
+
+                var parser = new DOMParser();
+                var doc = parser.parseFromString(htmlString, "text/html");
+
+                var cms = doc.body.querySelectorAll('.__line[cmd-text]');
+                //console.log(cms);
+                cms.forEach(li => {
+                    var cmd_text = li.getAttribute('cmd-text');
+                    var lit = li.innerText || '';
+                    lit = lit.trim();
+                    if (lit.length > 0 && lit[0] == '^') lit = lit.substr(1);
+                    li.innerHTML = '^' + cmd_text + lit;
+                });
+                return doc;
+            }
+            return null;
+        },
+        domGetText: function(doc) {
+            var out = doc.body.innerText.split('^').join('\n');
+            var a = _.filter(out.split('\n'), x => x.trim().length > 0);
+            out = a.join('\n');
+            console.log(a);
+            return out;
+        },
         cmdCall: function(cmd) {
             var self = this;
             switch (cmd) {
@@ -123,30 +184,75 @@
                     self.$forceUpdate();
                     break;
                 case 'HEADING':
+                    if (self.mode != 'code') return;
+                    if (self.lineId_selected.length == 0) {
+                        self.__alert('Message', 'Please choose a line to insert image at there');
+                        return;
+                    }
+                    self.cmd_editHeading();
                     break;
                 case 'BOLD':
+                    if (self.mode != 'code') return;
+                    if (self.lineId_selected.length == 0) {
+                        self.__alert('Message', 'Please choose a line to insert image at there');
+                        return;
+                    }
                     break;
                 case 'REMOVE':
+                    if (self.mode != 'code') return;
+                    if (self.lineId_selected.length == 0) {
+                        self.__alert('Message', 'Please choose a line to insert image at there');
+                        return;
+                    }
                     break;
                 case 'INSERT_IMAGE':
+                    if (self.mode != 'code') return;
+                    if (self.lineId_selected.length == 0) {
+                        self.__alert('Message', 'Please choose a line to insert image at there');
+                        return;
+                    }
+                    self.cmd_insertImage();
                     break;
                 case 'INSERT_VIDEO':
+                    if (self.mode != 'code') return;
+                    if (self.lineId_selected.length == 0) {
+                        self.__alert('Message', 'Please choose a line to insert image at there');
+                        return;
+                    }
                     break;
                 case 'INSERT_LINK':
+                    if (self.mode != 'code') return;
+                    if (self.lineId_selected.length == 0) {
+                        self.__alert('Message', 'Please choose a line to insert image at there');
+                        return;
+                    }
                     break;
             }
         },
-        updateHeading: function() {
-
+        cmd_editHeading: function() {
+            var self = this, el = self.$el, view_id = el.getAttribute('id');
+            var doc = self.domVirtualBuild();
+            if (doc) {
+                var line = doc.getElementById(self.lineId_selected);
+                if (line) {
+                    var isHeading = line.className.indexOf('h3') != -1;
+                    var s = line.innerText || '';
+                    s = s.trim();
+                    if (s[0] == '^') s = s.substr(1).trim();
+                    console.log(s);
+                    if (isHeading) {
+                        s = s.substr(3);
+                        line.innerText = s;
+                    }
+                    else  line.innerText = '^{H}' + s;
+                }
+                var out = self.domGetText(doc);
+                self.article.data = out;
+                self.$forceUpdate();
+            }
         },
         cmd_insertImage: function() {
             var self = this, el = self.$el, view_id = el.getAttribute('id');
-
-            if (self.lineId_selected.length == 0) {
-                self.__alert('Message', 'Please choose a line to insert image at there');
-                return;
-            }
-
             __vcp({
                 code: 'image-select',
                 scope: __scope,
@@ -155,78 +261,25 @@
                 title: 'Insert Image: ' + self.article.title,
                 class: 'ui overlay fullscreen modal',
                 multi_select: true
-            }, null, function (v) {
-
-            }, function (data) {
+            }, null, null, function (data) {
                 var imgs = _.map(_.filter(data.images, x => x.select), x => x.src);
-                console.log('close = ', view_id, imgs);
+                //console.log('close = ', view_id, imgs);
 
                 if (imgs.length > 0) {
                     imgs.forEach(x => {
                         self.article.images.push(x);
                     });
 
-                    var edit = document.getElementById('edit-html');
-                    if (edit) {
-                        var htmlString = edit.innerHTML;
-                        htmlString = htmlString
-                            .split('<p ').join('^<p ')
-                            .split('<br ').join('^<br ')
-                            .split('>').join('>^');
-
-                        var parser = new DOMParser();
-                        var doc = parser.parseFromString(htmlString, "text/html");
-
-                        var cms = doc.body.querySelectorAll('.__line[cmd-text]');
-                        console.log(cms);
-                        cms.forEach(li => {
-                            var cmd_text = li.getAttribute('cmd-text');
-                            var lit = li.innerText || '';
-                            lit = lit.trim();
-                            if (lit.length > 0 && lit[0] == '^') lit = lit.substr(1);
-                            li.innerHTML = '^' + cmd_text + lit;
-                        });
-
+                    var doc = self.domVirtualBuild();
+                    if (doc) {
                         var line = doc.getElementById(self.lineId_selected);
-                        if (line) line.innerHTML = imgs.join('| ');
-
-                        var out = doc.body.innerText.split('^').join('\n');
-                        var a = _.filter(out.split('\n'), x => x.trim().length > 0);
-                        //console.log(a);
-
-                        self.article.data = a.join('\n');
+                        if (line) line.innerHTML = imgs.join(' | ');
+                        var out = self.domGetText(doc);
+                        self.article.data = out;
+                        self.$forceUpdate();
                     }
                 }
             });
         },
-        openPopupBrowserImage: function() {
-            var self = this, el = self.$el, view_id = el.getAttribute('id');
-
-            __vcp({
-                code: 'image-select',
-                scope: __scope,
-                popup: true,
-                view_ref: view_id,
-                title: 'Images: ' + self.article.title,
-                class: 'ui overlay fullscreen modal',
-                multi_select: false
-            }, null, function (v) {
-
-            }, function (data) {
-                var imgs = _.map(_.filter(data.images, x => x.select), x => x.src);
-                console.log('close = ', view_id, imgs, self.article);
-
-                if (imgs.length > 0) {
-                    imgs.forEach(x => {
-                        if (self.article.images.length == 0) self.article.images.push(x);
-                        else self.article.images.unshift(x);
-                    });
-                }
-
-                //$('#' + view_id).modal('show');
-                //$('#' + view_id + ' .dimmer').removeClass('active');
-            });
-
-        }
     }
 }
